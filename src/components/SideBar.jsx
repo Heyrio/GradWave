f52b73e29c16180050f5968314c0ae6734d1b921
hamzah <hamzah@somaglobal.com>
2019-12-23 10:21:56 -0500

    building out shell structure

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function SideBar() {
     return (
         <div>
-            
+              USER PROFILE SIDE BAR GOES HERE Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+              Eos ducimus fuga accusantium. Ipsam, vero rerum placeat minima earum au
+              tem rem laboriosam aut nihil maxime delectus exercitationem aperiam. Recusandae, illo adipisci.
         </div>
     )
 }
--- src/containers/MainUserContainer.jsx
+++ src/containers/MainUserContainer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import HeaderBar from '../components/HeaderBar';
+import SideBar from '../components/SideBar'
+import { Grid } from '@material-ui/core';
 
 export default function MainUserContainer() {
     return (
         <div>
-            
+            <HeaderBar/>
+                <Grid container spacing={1}>
+                    <Grid container item lg={3} spacing={3}>
+                        <SideBar/>
+                    </Grid>
+                    <Grid container item xs={9} spacing={3}>
+                    two col
+                    </Grid>
+                </Grid>
         </div>
     )
 }
