429f76de8a6dae7331b9a0e5527fcb8e3e5ebb88
hamzah <hamzah@somaglobal.com>
2019-12-29 12:39:39 -0500

    more styling of the login page

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import {styles} from './Login.styles.js'
+import { Paper } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 function Login(props) {
     const {classes} = props;
     return (
         <div className={classes.loginContainer}>
             <div className={classes.loginTitle}>
-                <h1>GradWave </h1>
+                <h1>Grad <div className={classes.waveIcon}></div> Wave </h1>
+            </div>
+            <div>
+            <Paper className={classes.loginCard} variant="outlined">
+                Login
+            </Paper>
             </div>
         </div>
     )
--- src/containers/Login.styles.js
+++ src/containers/Login.styles.js
@@ -1,11 +1,12 @@
 import Background from '../components/images/loginbg.jpeg';
 import color from '@material-ui/core/colors/amber';
 import { positions, fontSize } from '@material-ui/system';
+import WaveIcon from '../components/images/waveIcon.png';
 
 export const styles = theme => ({
     loginContainer:{
         backgroundSize: "cover",
-        backgroundImage:  `url(${Background})`,
+        backgroundImage: "linear-gradient( rgba(0,0,0,0.2), rgba(0, 0, 0, 0.5) ),"+`url(${Background})`,
         height: "100vh",
         top: 0,
         left: 0,
@@ -13,11 +14,30 @@ export const styles = theme => ({
         right: 0,
         position: "absolute",
         zIndex: -1,
+        paddingLeft: "25%",
+        paddingRight: "25%",
+
     },
     loginTitle:{
-        color: "#488286",
+        color: "#077B75",
         fontSize: 40,
-        textShadow: "2px 2px 4px #000000",
+        // textShadow: "1px 1px 2px #000000",
+        textAlign: "center",
+    },
+
+    loginCard:{
+        textAlign: "center",
+        height: "50vh",
+        backgroundColor: "#fff"
+    },
+
+    waveIcon:{
+        display: "inline-flex",
+        backgroundSize: "cover",
+        backgroundImage: `url(${WaveIcon})`,
+        height: 100,
+        width: 125
     }
+
   
 });
