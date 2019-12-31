7bb36444b48fc59830f38d6ff629701e6d571e35
hamzah <hamzah@somaglobal.com>
2019-12-30 20:33:04 -0500

    styling main comp

--- src/containers/MainUserContainer.jsx
+++ src/containers/MainUserContainer.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import HeaderBar from '../components/HeaderBar';
 import SideBar from '../components/SideBar'
 import { Grid } from '@material-ui/core';
+import { classes } from 'istanbul-lib-coverage';
+import { withStyles } from '@material-ui/core/styles';
+import {styles} from './MainUserContainer.styles.js'
 
 //  This container will be connected to Redux
 
-export default function MainUserContainer() {
+function MainUserContainer(props) {
+
+    const { classes } = props;
+
     return (
         <div>
             <Grid container>
@@ -17,12 +23,23 @@ export default function MainUserContainer() {
                     <Grid container item lg={3} spacing={3}>
                         <SideBar/>
                     </Grid>
-                    <Grid container item xs={9} spacing={3}>
-                    two col Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempore ipsum exercitationem alias 
-                    debitis saepe dignissimos quod? Adipisci tempora 
-                    nostrum ex blanditiis deleniti harum rem quidem unde? Doloremque, harum repellendus.
+                    <Grid container className={classes.mainFeed} item xs={9} spacing={3}>
+                        <Grid item lg={12}>
+                            two col Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempore ipsum exercitationem alias 
+                            debitis saepe dignissimos quod? Adipisci tempora 
+                            nostrum ex blanditiis deleniti harum rem quidem unde? Doloremque, harum repellendus.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio sed voluptates ullam reiciendis laboriosam,
+                             quo voluptate porro vero error commodi sunt. Eum, facere perferendis magni quia saepe debitis voluptates nesciunt!
+                             two col Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempore ipsum exercitationem alias 
+                            debitis saepe dignissimos quod? Adipisci tempora 
+                            nostrum ex blanditiis deleniti harum rem quidem unde? Doloremque, harum repellendus.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio sed voluptates ullam reiciendis laboriosam,
+                             quo voluptate porro vero error commodi sunt. Eum, facere perferendis magni quia saepe debitis voluptates nesciunt!
+                        </Grid>
                     </Grid>
                 </Grid>
         </div>
     )
 }
+
+export default withStyles(styles)(MainUserContainer);
